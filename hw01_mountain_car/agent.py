297a0756c90eddef987b911f46db401d3a4b86c6
Egor Spirin <spirin.egor@mail.ru>
2021-02-10 20:29:31 +0300

    Implement Q-learning algorithm

--- hw01_mountain_car/agent.py
+++ hw01_mountain_car/agent.py
@@ -1,17 +1,19 @@
-import random
+from os.path import join
+
 import numpy as np
-import os
+
 from .train import transform_state
 
 
 class Agent:
-    def __init__(self):
-        self.qlearning_estimate = np.load(__file__[:-8] + "/agent.npz")
+    def __init__(self, path="agent.npy"):
+        full_path = join(__file__[:-8], path)
+        with open(full_path, "rb") as f_in:
+            self.qlearning_estimate = np.load(f_in)
         
     def act(self, state):
         state = transform_state(state)
-        return 0
+        return self.qlearning_estimate[state].argmax()
 
     def reset(self):
         pass
-
--- hw01_mountain_car/train.py
+++ hw01_mountain_car/train.py
@@ -1,86 +1,109 @@
-from gym import make
-import numpy as np
-import torch
-import copy
-from collections import deque
 import random
 
-GAMMA = 0.98
+import numpy as np
+from gym import make
+
+SEED = 7
 GRID_SIZE_X = 30
 GRID_SIZE_Y = 30
+
+TRANSITIONS = 4_000_000
+START_EPS = 0.3
+REWARD_GAMMA = 0.98
+
+GAMMA = 0.98
+ALPHA = 0.1
 
 
 # Simple discretization 
 def transform_state(state):
     state = (np.array(state) + np.array((1.2, 0.07))) / np.array((1.8, 0.14))
-    x = min(int(state[0] * GRID_SIZE_X), GRID_SIZE_X-1)
-    y = min(int(state[1] * GRID_SIZE_Y), GRID_SIZE_Y-1)
-    return x + GRID_SIZE_X*y
+    x = min(int(state[0] * GRID_SIZE_X), GRID_SIZE_X - 1)
+    y = min(int(state[1] * GRID_SIZE_Y), GRID_SIZE_Y - 1)
+    return x + GRID_SIZE_X * y
 
 
 class QLearning:
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, state_dim, action_dim, gamma: float = 0.98, alpha: float = 0.1):
         self.qlearning_estimate = np.zeros((state_dim, action_dim)) + 2.
+        self._gamma = gamma
+        self._alpha = alpha
 
     def update(self, transition):
         state, action, next_state, reward, done = transition
-        pass
+        bellman = reward + self._gamma * self.qlearning_estimate[next_state].max()
+        self.qlearning_estimate[state][action] *= (1 - self._alpha)
+        self.qlearning_estimate[state][action] += self._alpha * bellman
 
     def act(self, state):
-        return 0
+        return self.qlearning_estimate[state].argmax()
 
     def save(self, path):
-        weight = np.array(self.weight)
-        bias = np.array(self.bias)
-        np.savez("agent.npz", weight, bias)
+        np.save(path, self.qlearning_estimate)
 
 
-def evaluate_policy(agent, episodes=5):
+def evaluate_policy(agent, episodes=5, render: bool = False):
     env = make("MountainCar-v0")
+    env.seed(SEED)
     returns = []
     for _ in range(episodes):
         done = False
         state = env.reset()
         total_reward = 0.
-        
+
         while not done:
             state, reward, done, _ = env.step(agent.act(transform_state(state)))
+            if render:
+                env.render()
             total_reward += reward
         returns.append(total_reward)
     return returns
 
 
-if __name__ == "__main__":
+def main():
     env = make("MountainCar-v0")
-    ql = QLearning(state_dim=GRID_SIZE_X*GRID_SIZE_Y, action_dim=3)
-    eps = 0.1
-    transitions = 4000000
+    env.seed(SEED)
+    np.random.seed(SEED)
+    random.seed(SEED)
+
+    ql = QLearning(state_dim=GRID_SIZE_X * GRID_SIZE_Y, action_dim=3, gamma=GAMMA, alpha=ALPHA)
+
+    state = env.reset()
+    table_state = transform_state(state)
+
     trajectory = []
-    state = transform_state(env.reset())
-    for i in range(transitions):
-        total_reward = 0
-        steps = 0
-        
-        #Epsilon-greedy policy
-        if random.random() < eps:
+    best_score = -1e9
+    eps = np.linspace(START_EPS, 0, TRANSITIONS)
+
+    for i in range(TRANSITIONS):
+        # Epsilon-greedy policy
+        if random.random() < eps[i]:
             action = env.action_space.sample()
         else:
-            action = ql.act(state)
+            action = ql.act(table_state)
 
         next_state, reward, done, _ = env.step(action)
-        reward += abs(next_state[1]) / 0.07
-        next_state = transform_state(next_state)
+        reward += 1000 * (REWARD_GAMMA * abs(next_state[1]) - abs(state[1]))
+        table_next_state = transform_state(next_state)
 
-        trajectory.append((state, action, next_state, reward, done))
-        
+        trajectory.append((table_state, action, table_next_state, reward, done))
+
         if done:
             for transition in reversed(trajectory):
                 ql.update(transition)
             trajectory = []
-        
-        state = next_state if not done else transform_state(env.reset())
-        
-        if (i + 1) % (transitions//100) == 0:
+
+        state = next_state if not done else env.reset()
+        table_state = transform_state(state)
+
+        if (i + 1) % (TRANSITIONS // 100) == 0:
             rewards = evaluate_policy(ql, 5)
-            print(f"Step: {i+1}, Reward mean: {np.mean(rewards)}, Reward std: {np.std(rewards)}")
-            ql.save()
+            mean, std = np.mean(rewards), np.std(rewards)
+            if mean > best_score:
+                ql.save("agent.npy")
+                best_score = mean
+            print(f"Step: {i + 1}, Reward mean: {mean}, Reward std: {std}")
+
+
+if __name__ == "__main__":
+    main()
